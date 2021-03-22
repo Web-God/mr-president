88ce0ee4d353574ffe82afcdec30a1d0b8d2a502
Denis Godec <denis.godec@weekub.fr>
2021-03-22 23:12:10 +0100

    clean code

--- assets/js/Game.js
+++ assets/js/Game.js
@@ -8,8 +8,8 @@ export function Game(options) {
 	this.cardNumber = 14;
 	this.duration = 1000;
 
-	this.hide = (elem) => elem.classList.add('hide');
-	this.show = (elem) => elem.classList.remove('hide');
+	this.hide = elem => elem.classList.add('hide');
+	this.show = elem => elem.classList.remove('hide');
 
 	this.seconds;
 	this.minutes;
@@ -288,9 +288,6 @@ Game.prototype.luckyGuess = function (e) {
 		points = points - 150;
 	}
 }
-
-
-
 
 
 // Enable Buttons
